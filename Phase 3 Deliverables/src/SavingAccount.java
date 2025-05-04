dafdb9e5d6b921e41f061a97047d9d4a03eac2be
Matthew Zhang <144151496+Matt952023@users.noreply.github.com>
2025-05-04 10:50:49 -0700

    Edited SavingsAccount
    Kept original methods, but add the functionality of resetting every month.

--- Phase 3 Deliverables/src/SavingAccount.java
+++ Phase 3 Deliverables/src/SavingAccount.java
@@ -3,33 +3,57 @@ import java.time.LocalDate;
 public class SavingAccount extends Account {
 	// Should reset withdrawCount every Month
 	private int withdrawCount; // counts current withdraws made to account
-	private static final int DEFAULT_MONTHLY_WITHDRAWAL_LIMIT = 5;
-    private LocalDate reset;
-
+	private int withdrawLimit; // maximum number of withdraws that can be made
 	
-	public SavingAccount() {
-		super();
+	// default if no limit is specified
+    private static final int DEFAULT_MONTHLY_WITHDRAWAL_LIMIT = 5;
+    // tracks when we last zeroed out withdrawCount
+    private LocalDate lastReset;
+    
+    public SavingAccount() {
+    	// uses the other constructor, but with the default limit (wow)
+    	this(DEFAULT_MONTHLY_WITHDRAWAL_LIMIT);
 	}
 	
+	public SavingAccount(int limit) {
+		super();
+		withdrawLimit = limit;
+		this.lastReset = LocalDate.now();
+        this.withdrawCount = 0;
+	}
+	
+	public void setWithdrawLimit(int limit) {
+		this.withdrawLimit = limit;
+	}
+	public int getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	public void setWithdrawCount(int withdrawCount2) {
+		// ensure count is up-to-date before showing
+        checkReset();
+		this.withdrawCount = withdrawCount2;	
+	}
 	public int getWithdrawCount() {
 		return withdrawCount;
 	}
-
-	// resets every new calendar month
-	private void checkReset() {
+	/**-- Internal reset logic --**/
+    private void checkReset() {
         LocalDate now = LocalDate.now();
-        if (now.getMonth() != reset.getMonth() || now.getYear() != reset.getYear()) {
+        if (now.getYear() != lastReset.getYear() ||
+            now.getMonth() != lastReset.getMonth()) {
             withdrawCount = 0;
-            reset = now;
+            lastReset = now;
         }
     }
 
-	@Override
+    /**-- Main transaction hook --**/
+    @Override
     public void addTransaction(Transaction trans) {
-		checkReset();
-		
+        // reset if we've crossed into a new month
+        checkReset();
+
         if (trans.getOperation() == Transaction.OPERATION.WITHDRAW) {
-            if (withdrawCount >= DEFAULT_MONTHLY_WITHDRAWAL_LIMIT) {
+            if (withdrawCount >= withdrawLimit) {
                 throw new IllegalStateException("Withdrawal limit reached");
             }
             withdrawCount++;
