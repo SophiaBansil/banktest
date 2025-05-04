7243881264283046b7e605f7f7421fa3947f5e12
Matthew Zhang <144151496+Matt952023@users.noreply.github.com>
2025-05-03 20:36:22 -0700

    Adjusted Central Server
    Added handleCreateProfile()
    Added handleCreateAccount()
    Added additional fields for username in account
    Added additional fields for withdraw and designated account in profile message
    Altered handleTransaction() to give an exception on failed transaction for savings and line of credit

--- Phase 3 Deliverables/src/Message.java
+++ Phase 3 Deliverables/src/Message.java
@@ -5,26 +5,25 @@ public abstract class Message implements Serializable {
 	public enum TYPE {
 		LOGIN_CLIENT,
 		LOGIN_TELLER,
-		LOGOUT_ATM, 
-	    LOGOUT_CLIENT, // not needed?
+		LOGOUT_ATM,
+	    LOGOUT_CLIENT,
 	    LOGOUT_TELLER,
 	    SUCCESS,
 	    FAILURE,
 	    LOAD_ACCOUNT,
-	    LOAD_PROFILE,
-	    SAVE_PROFILE,
+	    CREATE_ACCOUNT,
 	    SAVE_ACCOUNT,
 	    SHARE_ACCOUNT,
 	    DELETE_ACCOUNT,
+	    LOAD_PROFILE,
+	    SAVE_PROFILE,
+	    CREATE_PROFILE,
 	    DELETE_PROFILE,
 	    EXIT_ACCOUNT,
 	    EXIT_PROFILE,
-		CREATE_ACCOUNT,
-		CHECK_USERNAME_AVAILABILITY,
-		CREATE_PROFILE,
 	    TRANSACTION,
-	    SHUTDOWN,
-		SESSION_TIMEOUT // implement
+	    SHUTDOWN, 
+	    CHECK_USERNAME_AVAILABILITY
 	}
 	
 	private final TYPE type;
